aea099abe82210b6919629edd7131cc5423dc616
David Noyes <david.j.noyes@gmail.com>
2014-12-22 17:54:30 +0000

    Packaged and interfaced

---
+++ consulResolver/client.go
@@ -0,0 +1,70 @@
+package consulResolver
+
+import (
+	"fmt"
+	"github.com/armon/consul-api"
+	//log "github.com/golang/glog"
+	"strings"
+)
+
+type Client struct {
+	kv     *consulapi.KV
+	prefix string
+}
+
+func NewConsulClient(address, prefix string) (*Client, error) {
+	config := consulapi.DefaultConfig()
+	config.Address = address + ":8500"
+	client, err := consulapi.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+	kv := client.KV()
+	return &Client{
+		kv:     kv,
+		prefix: prefix,
+	}, nil
+}
+
+func (client *Client) GetValue(key string) (value string) {
+	kvPair, _, err := client.kv.Get(client.prefix+key, nil)
+	if err != nil {
+		return
+	}
+	if kvPair == nil {
+		return
+	}
+	value = fmt.Sprintf("%s", kvPair.Value)
+	return
+}
+
+func (client *Client) GetKeys(path string) []string {
+	keys, _, _ := client.kv.Keys(client.prefix+path, "/", nil)
+	return keys
+}
+
+func (client *Client) GetChildKeys(path string) (childKeys []string) {
+	keys := client.GetKeys(path)
+	for i, _ := range keys {
+		keys[i] = strings.TrimPrefix(keys[i], client.prefix+path)
+		keys[i] = strings.TrimSuffix(keys[i], "/")
+		if keys[i] != "" {
+			childKeys = append(childKeys, keys[i])
+		}
+	}
+	return
+}
+
+func (client *Client) KeyExists(key string) bool {
+	keys, _, _ := client.kv.Keys(client.prefix+key, "/", nil)
+	return len(keys) > 0
+}
+
+func (client *Client) GetChildKeyValues(path string) (results map[string]string) {
+	results = make(map[string]string)
+	keys := client.GetChildKeys(path)
+	for _, key := range keys {
+		results[key] = client.GetValue(path + key)
+	}
+	return
+}
--- pipe-consul.go
+++ pipe-consul.go
@@ -1,66 +1,27 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"consul"
-	"errors"
+	"./consulResolver"
+	"./pdns"
 	"flag"
-	"fmt"
 	log "github.com/golang/glog"
-	"hash/fnv"
-	"io"
 	"os"
-	"pdns"
 )
 
 const (
-	END_REPLY        = "END\n"
-	FAIL_REPLY       = "FAIL\n"
-	GREETING_REPLY   = "OK\tpipe-consul\n"
-	TAG_AXFR         = "AXFR" // ignored for now
-	TAG_PING         = "PING"
-	TAG_Q            = "Q"
-	defaultScopebits = "0"
-	defaultAuth      = "1"
-	defaultTTL       = "500" //5 mins
-	defaultId        = "2"
-	defaultPriority  = 0
-	defaultWeight    = 0
+	defaultTTL = "500" //5 mins
 )
-
-var (
-	GREETING_ABI_V1 = []byte("HELO\t1")
-	GREETING_ABI_V2 = []byte("HELO\t2")
-	GREETING_ABI_V3 = []byte("HELO\t3")
-	GREETING_ABI_V4 = []byte("HELO\t4")
-	errLongLine     = errors.New("pdns line too long")
-	errBadLine      = errors.New("pdns line unparseable")
-)
-
-type question struct {
-	tag      string
-	qname    string
-	qclass   string // always "IN"
-	qtype    string // almost always "ANY"
-	id       string
-	remoteIp string
-}
-
-func (res *result) formatResult() string {
-	return fmt.Sprintf("DATA\t%v\t%v\t%v\t%v\t%v\t%v\n", res.qname, res.qclass, res.qtype, res.ttl, res.id, res.content)
-}
 
 func main() {
 	environment := flag.String("environment", "", "The name of the environment being served for")
 	consulConn := flag.String("address", "", "The URL to the Consul service")
 	flag.Parse()
-	cRes, err := newConsulResolver(*environment, *consulConn)
+	cRes, err := consulResolver.New(*environment, *consulConn, defaultTTL)
 	if err != nil {
 		log.Errorf("FAIL %s", err)
 		os.Exit(1)
 	}
-	pd := &pdns{cRes, os.Stdin, os.Stdout}
+	pd := &pdns.PDNS{cRes, os.Stdin, os.Stdout}
 	pd.Process()
 	os.Stdout.Close()
 }
